b896e966b6f7915a03005ca387ead8cb860bcd76
estudiantes <estudiantes@labing00>
2017-03-08 18:16:57 -0500

    Commit equipos de futbol

---
+++ Arreglos/src/arreglos/Fanatico.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Fanatico {
+    String nombre;
+    int puñaladas;
+    int edad;
+    float porcentajeFanatismo;
+    
+}
